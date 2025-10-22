6f75c63ce9a295d37d010acb19980d0f85487bf5
Zach <zwright5270@gmail.com>
2025-10-22 19:12:52 -0400

    reverting PPR

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental' // Allows to adopt PPR for specific routes
-  }
 };
 
 export default nextConfig;
